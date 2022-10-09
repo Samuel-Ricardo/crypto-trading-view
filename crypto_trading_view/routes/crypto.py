e3465f820ac78dc3f0f057d5d115d6b84401f167
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-10-09 17:20:25 -0300

    :sparkles: create: def - daily_report (routes.crypto)

--- crypto_trading_view/routes/crypto.py
+++ crypto_trading_view/routes/crypto.py
@@ -1,6 +1,10 @@
-
+from connections.crypto import day_summary, day_summary_of
 from fastapi import APIRouter
 
 
 crypto_routes = APIRouter(prefix='/crypto')
 
+
+@crypto_routes.get('/daily_report/{user_id}')
+async def daily_report(user_id:int):
+    return await day_summary(user_id)
