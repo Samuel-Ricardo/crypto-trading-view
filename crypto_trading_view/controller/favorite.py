b78e1f0ef7f39fe224d6500d91a532b00db086dc
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-10-09 16:15:42 -0300

    :sparkles: create: def - remove_of (controller.favorite)

--- crypto_trading_view/controller/favorite.py
+++ crypto_trading_view/controller/favorite.py
@@ -1,11 +1,22 @@
 from fastapi import HTTPException
-from service._ import add_favorite
-from schema.index import UserAddFavoriteInput, StandardOutput
+from schema.index import UserAddFavoriteInput,\
+                         StandardOutput
+
+from service._ import add_favorite,\
+                      remove_favorite
 
 
 async def add_to(props: UserAddFavoriteInput):
     try:
         await add_favorite(user_id = props.user_id, symbol = props.symbol)
         return StandardOutput(message='OK')
+    except Exception as error:
+        raise HTTPException(400, detail=str(error))
+
+
+async def remove_of(user_id: int, symbol: str):
+    try:
+        await remove_favorite(user_id, symbol)
+        return StandardOutput(message='Ok')
     except Exception as error:
         raise HTTPException(400, detail=str(error))
