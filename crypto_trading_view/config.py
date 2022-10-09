22d2b07eb578e5cc1d124ed2e821db395e24818e
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-10-09 14:46:15 -0300

    :gear: update: config.py

--- crypto_trading_view/config.py
+++ crypto_trading_view/config.py
@@ -1,4 +1,8 @@
+from datetime import date
 from os import getenv
 
 
 DATABASE_URL = getenv('DATABASE_URL')
+
+def MERCADO_BITCOIN_API_URL(symbol: str, date: date) -> str:
+    return f'https://www.mercadobitcoin.net/api/{symbol}/day-summary/{date.year}/{date.month}/{date.day}/'
