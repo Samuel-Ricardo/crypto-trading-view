36bbe09492aedbc04b906a1812e62fe9bd824704
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-10-08 22:28:38 -0300

    :sparkles: create: model - Favorite (database)

---
+++ crypto_trading_view/database/model/favorite.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from index import Base
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    symbol = Column(String)
+    user_id = Column(Integer, ForeignKey('user.id'))
