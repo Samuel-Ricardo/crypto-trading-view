b2619bb9b3a9b8966beafe691958ee49eddeb965
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-10-09 13:53:44 -0300

    :sparkles: create: fun - delete_user (service.user)

--- crypto_trading_view/service/user.py
+++ crypto_trading_view/service/user.py
@@ -1,7 +1,13 @@
+from sqlalchemy import delete
 from database.model.index import User
 from sqlalchemy.ext.asyncio.session import async_session
 
 async def create_user(name: str):
     async with async_session() as session:
         session.add(User(name=name))
+        await session.commit()
+
+async def delete_user(user_id: int):
+    async with async_session() as session:
+        await session.execute(delete(User).wwhere(User.id == user_id))
         await session.commit()
