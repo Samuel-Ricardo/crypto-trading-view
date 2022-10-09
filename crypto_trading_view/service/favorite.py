a539804740324df8178386c7c9bc248c470c5339
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-10-09 14:21:39 -0300

    :sparkles: create: def - remove-favorite (service.favorite)

--- crypto_trading_view/service/favorite.py
+++ crypto_trading_view/service/favorite.py
@@ -1,7 +1,13 @@
+from sqlalchemy import delete
 from database.model.index import Favorite
 from sqlalchemy.ext.asyncio.session import async_session
 
 async def add_favorite(user_id: int, symbol: str):
     async with async_session() as session:
         session.add(Favorite(user_id=user_id, symbol=symbol))
+        await session.commit()
+
+async def remove_favorite(user_id: int, symbol: str):
+    async with async_session() as session:
+        await session.execute(delete(Favorite).where(Favorite.user_id == user_id, Favorite.symbol == symbol))
         await session.commit()
