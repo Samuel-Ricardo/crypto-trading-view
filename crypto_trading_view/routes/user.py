a8c2e47b151b2a26da48dc0ebf0fbfda43677773
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-10-09 16:45:54 -0300

    :sparkles: create: def - create_user (routes.user)

---
+++ crypto_trading_view/routes/user.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+from controller.user import create, delete,  list
+from schema.commons_schema import StandardOutput, ErrorOutput
+from schema.user_schema import UserCreateInput, UserDeleteInput, UserListOutput
+
+user_router = APIRouter(prefix='/user')
+
+@user_router.post('/create', description='My description', response_model=StandardOutput, responses= {400: {'model': ErrorOutput}})
+async def create_user(input: UserCreateInput):
+    return create(input)
