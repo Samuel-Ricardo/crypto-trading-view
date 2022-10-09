c66528b7f0e4a319b6ebc08d45af6c10764e7a7a
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-10-08 21:49:53 -0300

    :sparkles: create: schema - UserAddFavoriteInput

--- crypto_trading_view/schema/user_schema.py
+++ crypto_trading_view/schema/user_schema.py
@@ -1,8 +1,14 @@
 from pydantic import BaseModel
 
+import symbol
+
 
 class UserCreateInput(BaseModel):
     name: str
 
 class UserDeleteInput(BaseModel):
     id: int
+
+class UserAddFavoriteInput(BaseModel):
+    user_id: int
+    symbol: str
