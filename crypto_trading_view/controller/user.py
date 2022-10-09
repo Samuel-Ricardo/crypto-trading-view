d4af550efa5dfee7dfb28508fbeb3dea90d5d0b5
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-10-09 16:00:05 -0300

    :sparkles: create: def - delete (controller.user)

--- crypto_trading_view/controller/user.py
+++ crypto_trading_view/controller/user.py
@@ -1,10 +1,18 @@
 from fastapi import HTTPException
-from service._ import create_user
-from schema.index import UserCreateInput, StandardOutput
+from service._ import create_user, delete_user
+from schema.index import UserCreateInput, UserDeleteInput, StandardOutput
 
-async def create_user(input: UserCreateInput):
+async def create(input: UserCreateInput):
     try:
         await create_user(name=input.name)
+        return StandardOutput(message='OK')
+    except Exception as error:
+        raise HTTPException(400, detail=str(error))
+
+
+async def delete(input: UserDeleteInput):
+    try:
+        await delete_user()
         return StandardOutput(message='OK')
     except Exception as error:
         raise HTTPException(400, detail=str(error))
