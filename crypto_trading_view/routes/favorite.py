bf1719e51a53c9f66774e085d3d30a19fd6ab05a
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-10-10 08:05:04 -0300

    :sparkles: create: def - add_favorite (routes.favorite)

--- crypto_trading_view/routes/favorite.py
+++ crypto_trading_view/routes/favorite.py
@@ -1,6 +1,15 @@
-
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from schema.index import StandardOutput, ErrorOutput, UserAddFavoriteInput
+from service.favorite import add_favorite
 
 
 favorite_routes = APIRouter(prefix='/favorites')
+
+@favorite_routes.post('/add', response_model=StandardOutput, responses={400: {'model': ErrorOutput}})
+async def add_favorite(input: UserAddFavoriteInput):
+    try:
+        await add_favorite(input.user_id, input.symbol)
+        return StandardOutput(message='Ok')
+    except Exception as error:
+        raise HTTPException(400, detail=str(error))
 
