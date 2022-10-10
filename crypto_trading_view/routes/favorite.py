76ce9d883f4e5c8613244ecc70f21b7e3bbaee16
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-10-10 08:08:03 -0300

    :sparkles: create: def - remove_favorite (routes.favorite)

--- crypto_trading_view/routes/favorite.py
+++ crypto_trading_view/routes/favorite.py
@@ -13,3 +13,11 @@ async def add_favorite(input: UserAddFavoriteInput):
     except Exception as error:
         raise HTTPException(400, detail=str(error))
 
+
+@favorite_routes.delet('/{user_id}', response_model=StandardOutput, responses= {400: {'model': ErrorOutput}})
+async def remove_favorite(user_id: int, symbol: str):
+    try:
+        await remove_favorite(user_id, symbol)
+        return StandardOutput(message='Ok')
+    except Exception as error:
+        raise HTTPException(400, detail=str(error))
